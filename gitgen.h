67e180a93ddf4db0e4c442b70ee6b7bc9458f907
Helios <35069323+wlhcode@users.noreply.github.com>
2019-01-20 12:54:29 +0800

    Add files via upload

---
+++ gitgen.h
@@ -0,0 +1,19 @@
+#ifndef _GITGEN_H
+#define _GITGEN_H
+
+#include <iostream>
+#include <algorithm>
+#include <ctime>
+#include <cstdlib>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <windows.h>
+
+int ans[15][15];
+int pro[15][15];
+int diff;
+void thegreatfunction();
+
+#endif
